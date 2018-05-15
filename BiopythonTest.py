3d69b3b1a9d953060ed22c13f6192ef2ac0896ff
akankhyab <abehera@luc.edu>
2018-05-15 15:21:39 -0500

    new changes

--- BiopythonTest.py
+++ BiopythonTest.py
@@ -2,7 +2,7 @@ import Bio
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_protein, generic_dna
 
-testSeq = Seq("GTACCGTCA", generic_dna)
+testSeq = Seq("GTACCGTCA", generic_protein)
 
 print(testSeq)
 print(testSeq.alphabet)
